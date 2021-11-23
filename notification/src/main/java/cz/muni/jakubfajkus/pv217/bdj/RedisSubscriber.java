3ef52ab9e4fa3d6be02d26a80f7f4540cc43b1ff
Jakub-Fajkus <jakub.fajkus@gmail.com>
2021-11-23 18:46:51 +0100

    fix connection to redis in notifications-service

--- notification/src/main/java/cz/muni/jakubfajkus/pv217/bdj/RedisSubscriber.java
+++ notification/src/main/java/cz/muni/jakubfajkus/pv217/bdj/RedisSubscriber.java
@@ -8,12 +8,14 @@ import io.vertx.redis.client.Command;
 import io.vertx.redis.client.Redis;
 import io.vertx.redis.client.RedisOptions;
 import io.vertx.redis.client.Request;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import java.util.List;
 
 @ApplicationScoped
 public class RedisSubscriber {
@@ -23,20 +25,28 @@ public class RedisSubscriber {
 
     private static final Logger log = LoggerFactory.getLogger(RedisSubscriber.class);
 
+    @ConfigProperty(name = "quarkus.redis.notifications.hosts")
+    private String connectionString;
 
     public void subscribeNotifications(@Observes StartupEvent ev) {
-        Redis.createClient(vertx, new RedisOptions())
+        log.info("The app is starting... in subscribeNotifications");
+        RedisOptions options = new RedisOptions();
+        options.setEndpoints(List.of(connectionString));
+
+        Redis.createClient(vertx, options)
                 .connect()
                 .onSuccess(conn -> {
                     conn
                             .send(Request.cmd(Command.SUBSCRIBE).arg("notifications"))
-                            .onSuccess(response -> log.info("Subscribed to notifications " + response));
+                            .onSuccess(response -> log.info("Subscribed to notifications " + response))
+                            .onFailure(response -> log.info("Failed to subscribe " + response));
 
                     conn.handler(message -> {
                         if (message.size() == 3 && message.get(1).toString().equals("notifications")) {
                             log.info("Notification received: " + message.get(2));
                         }
                     });
-                });
+                })
+                .onFailure(response -> log.error("Failed to connect to redis: " + response));
     }
 }
